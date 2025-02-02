370876071b7a1d71fbbc25567074abfc881c7d8a
Katarzyna Koloda <koloda.katarzyna@gmail.com>
2025-02-02 21:31:18 +0100

    added docs and type annotations

--- Food101/src/training_evaluation.py
+++ Food101/src/training_evaluation.py
@@ -1,4 +1,5 @@
 import json
+from typing import Tuple, Optional, List, Dict
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,8 +18,11 @@ INITIAL_EPOCHS = 3
 NUM_CLASSES = 101
 
 
-def build_feature_extraction_model(base_model_class, input_shape=(IMG_SIZE, IMG_SIZE, 3),
-                                   preprocess_layer=None, num_classes=NUM_CLASSES):
+def build_feature_extraction_model(base_model_class: tf.keras.Model,
+                                   input_shape: Tuple[int, int, int] = (IMG_SIZE, IMG_SIZE, 3),
+                                   preprocess_layer: Optional[layer.Layer]= None,
+                                   num_classes: int = NUM_CLASSES
+                                  ):
     """
     Args:
         base_model_class: A class for the base model (e.g., tf.keras.applications.MobileNet).
@@ -48,8 +52,21 @@ def build_feature_extraction_model(base_model_class, input_shape=(IMG_SIZE, IMG_
 
 
 
-def plot_loss_curves(history, title, test_name=""):
-    """Return separate loss/accuracy curves for training and validation metrics."""
+def plot_loss_curves(history: callbacks.History,
+                     title: str,
+                     test_name: Optional[str] = None
+                    ) -> None:
+    """
+    Plot separate loss and accuracy curves for training and validation metrics.
+
+    Args:
+        history (tf.keras.callbacks.History): Training history object from a Keras model.
+        title (str): Title for the plots.
+        test_name (str, optional): If provided, saves the plot with this name.
+
+    Returns:
+        None: Displays the plots and optionally saves them as a file.
+    """
     loss = history.history["loss"]
     val_loss = history.history["val_loss"]
 
@@ -81,19 +98,36 @@ def plot_loss_curves(history, title, test_name=""):
     plt.show()
     
 
-def test_models(models_config, train_data, validation_data, test_data, num_classes=NUM_CLASSES, epochs=INITIAL_EPOCHS):
+def test_models(models_config: List[Dict],
+                train_data: tf.data.Dataset,
+                validation_data: tf.data.Dataset,
+                test_data: tf.data.Dataset,
+                num_classes: int = NUM_CLASSES,
+                epochs: int = INITIAL_EPOCHS
+               ) -> pd.DataFrame:
     """
     Test various models with different configurations and save the results.
     
     Args:
-        models_config (list): List of dicts containing model, its name and preprocessor.
+        models_config (list): List of dicts containing model and preprocessor.
+        Example:
+                [
+                    {
+                        'model': applications.MobileNetV3Large,
+                        'preprocessor': applications.mobilenet_v3.preprocess_input
+                    },
+                    {
+                        'model': applications.EfficientNetV2S,
+                        'preprocessor': None
+                    }
+                    ]
         train_data (tf.data.Dataset): Training data.
         val_data (tf.data.Dataset): Validation data.
         num_classes (int): Number of output classes.
         epochs (int): Number of epochs to train the model.
     
     Returns:
-        pd.DataFrame: DataFrame with model performance results.
+        pd.DataFrame: DataFrame with models performance results.
     """
     results = []
 
@@ -140,16 +174,21 @@ def test_models(models_config, train_data, validation_data, test_data, num_class
     return results.sort_values(by="Test accuracy", ascending=False)
 
 
-def build_model_for_fine_tuning(base_model_class, input_shape=(IMG_SIZE, IMG_SIZE, 3), num_classes=NUM_CLASSES, unfrozen_layers=None):
+def build_model_for_fine_tuning(base_model_class: tf.keras.Model,
+                                input_shape: Tuple[int, int, int] = (IMG_SIZE, IMG_SIZE, 3),
+                                unfrozen_layers: Optional[int] = None
+                                ) -> tf.keras.Model:
     """
+    Builds a fine-tunable Keras model using a pre-trained base model.
+
     Args:
-        base_model_class: A class for the base model (e.g., tf.keras.applications.MobileNet).
-        input_shape: The shape of the input data (excluding batch size).
-        num_classes: The number of output classes for classification.
-        learning_rate: Learning rate for base optimizer (set to 10x lower than the default)
+        base_model_class (Type[tf.keras.Model]): A class for the base model (e.g., tf.keras.applications.MobileNet).
+        input_shape (tuple[int, int, int]): The shape of the input data (excluding batch size).
+        num_classes (int): The number of output classes for classification.
+        unfrozen_layers (Optional[int]): Number of last layers to unfreeze for fine-tuning (default is 10).
 
     Returns:
-        tf.keras.Model: A compiled Keras model.
+        tf.keras.Model: A Keras model (not compiled) ready for fine-tuning.
     """
     if base_model_class.__name__ == "MobineNetV3Large":
         x = layers.Lambda(applications.mobilenet_v3.preprocess_input)(layers.Input(shape=input_shape))
@@ -176,9 +215,26 @@ def build_model_for_fine_tuning(base_model_class, input_shape=(IMG_SIZE, IMG_SIZ
     return model
 
 
-def compare_histories(name, original_history_history_dict, new_history, test_name, initial_epochs=INITIAL_EPOCHS):
+def compare_histories(name: str,
+                      original_history_dict: Dict[str, list],
+                      new_history: callbacks.History,
+                      test_name: str,
+                      initial_epochs: int = INITIAL_EPOCHS
+                     ) -> None:
     """
-    Compares two TensorFlow training histories.
+    Merge and visualize training histories from feature extraction and fine-tuning.
+
+    This function combines the accuracy and loss from both stages and plots the comparison, showing the impact of fine-tuning on model performance.
+
+    Args:
+        name (str): Title for the plots.
+        original_history_dict (Dict[str, list]): Accuracy and loss history from feature extraction.
+        new_history (tf.keras.callbacks.History): History from fine-tuning.
+        test_name (str): Name for saving the plot.
+        initial_epochs (int, optional): Number of epochs before fine-tuning. Defaults to INITIAL_EPOCHS.
+
+    Returns:
+        None: Displays and saves the comparison plot.
     """
     acc = original_history_history_dict["accuracy"]
     loss = original_history_history_dict["loss"]
@@ -215,10 +271,35 @@ def compare_histories(name, original_history_history_dict, new_history, test_nam
     plt.show()
 
 
-def fine_tune_models(test_name, train_data, validation_data, test_data,
-                     model_list=[EfficientNetV2S, MobileNetV3Large], add_epochs=22, learning_rate=1e-04,
-                     load_previous_weights=False, initial_epoch=0, unfrozen_layers=None):
+def fine_tune_models(test_name: str,
+                     train_data: tf.data.Dataset,
+                     validation_data: tf.data.Dataset,
+                     test_data: tf.data.Dataset,
+                     model_list: List[tf.keras.Model] = [EfficientNetV2S, MobileNetV3Large],
+                     add_epochs:int = 22,
+                     learning_rate: float = 1e-04,
+                     load_previous_weights: bool = False,
+                     initial_epoch: int = 0,
+                     unfrozen_layers: Optional[int] = None
+                    ) -> pd.DataFrame:
+    """
+    Fine-tune a list of models on the given datasets and save the results.
 
+    Args:
+        test_name (str): Name of the test to identify saved files.
+        train_data (tf.data.Dataset): Training data.
+        validation_data (tf.data.Dataset): Validation data.
+        test_data (tf.data.Dataset): Test data.
+        model_list (List[tf.keras.Model], optional): List of models to fine-tune. Defaults to [EfficientNetV2S, MobileNetV3Large].
+        add_epochs (int, optional): Additional epochs for fine-tuning. Defaults to 22.
+        learning_rate (float, optional): Learning rate for the optimizer. Defaults to 1e-04.
+        load_previous_weights (bool, optional): Whether to load weights from previous training. Defaults to False.
+        initial_epoch (int, optional): Starting epoch for fine-tuning. Defaults to 0.
+        unfrozen_layers (Optional[int], optional): Number of layers to keep unfrozen during fine-tuning. Defaults to 10.
+
+    Returns:
+        pd.DataFrame: DataFrame with models' validation and test accuracies.
+    """
     results = []
 
     for model_class in model_list:
